c630142096bcb527becf1ee3252a571edd46d008
manngo <manngo2013@gmail.com>
2023-06-18 13:47:15 +0700

    Lecture 37: Using Material UI

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,78 @@
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import {
+  Avatar,
+  Box,
+  Button,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+function SignUp(props) {
+  return (
+    <div>
+      <Box>
+        <Avatar>
+          <AccountCircleIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <Box component="form">
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                name="firstName"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="password"
+                label="Password"
+                name="password"
+              />
+            </Grid>
+          </Grid>
+          <Button type="submit" fullWidth variant="contained">
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </div>
+  );
+}
+
+export default SignUp;
